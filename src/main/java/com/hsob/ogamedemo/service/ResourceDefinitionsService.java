29f74daa6a4473df268defb88881ab949f29bca0
Carlos Roberto <carlos.junior@hitechnologies.com.br>
2023-04-14 15:46:18 -0300

    testes do Calesco

--- src/main/java/com/hsob/ogamedemo/service/ResourceDefinitionsService.java
+++ src/main/java/com/hsob/ogamedemo/service/ResourceDefinitionsService.java
@@ -2,16 +2,42 @@ package com.hsob.ogamedemo.service;
 
 import com.hsob.ogamedemo.dto.resource_definitions.request.ResourceDefinitionsRequest;
 import com.hsob.ogamedemo.dto.resource_definitions.response.ResourceDefinitionsResponse;
+import com.hsob.ogamedemo.model.resources_definitions.Icon;
 import com.hsob.ogamedemo.model.resources_definitions.ResourceDefinitions;
+import com.hsob.ogamedemo.model.user.User;
 import com.hsob.ogamedemo.repository.ResourceDefinitionsRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ResourceDefinitionsService {
+
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        Icon icon = new Icon();
+        icon.setName("NOME DO ICONE");
+        icon.setType("ICONE TIPO");
+        icon.setSrc("SRC DO ICONE");
+
+        User user = new User();
+        user.setName("NOME DO USER");
+        user.setUsername("USERNAME DO USER");
+
+        var value = System.currentTimeMillis()%2==0 ? icon : user;
+        System.out.println(value.getClass().getName());
+        for(Method method : value.getClass().getDeclaredMethods()){
+            System.out.println(method.getName());
+            if(method.getName().contains("get")) {
+                System.out.println(method.invoke(value));
+            }
+        }
+
+    }
 
     @Autowired
     private ResourceDefinitionsRepository resourceDefinitionsRepository;
--- src/main/java/com/hsob/ogamedemo/service/UserService.java
+++ src/main/java/com/hsob/ogamedemo/service/UserService.java
@@ -12,6 +12,7 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
     public UserRequest createNewUser(UserRequest userRequest) {
         try {
             var user = new User(userRequest);
